7c7676b939f35b756b404a4e39d868b01470f7ec
Sanskriti singh <146508510+Sanskriti3104@users.noreply.github.com>
2026-01-16 14:56:56 +0530

    Add and Style weekly forecast

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const humidity = document.querySelector(".humidity");
 const wind = document.querySelector(".wind");
 const pressure = document.querySelector(".pressure");
 const uv = document.querySelector(".uv");
+const weeklyForecast = document.querySelector(".weekly-forecast");
+const dailyContainer = document.querySelector(".daily");
 
 searchBtn.addEventListener("click", getCity);
 
@@ -38,8 +40,45 @@ async function fetchWeatherData(cityName) {
         pressure.textContent = data.currentConditions.pressure + " hPa";
         uv.textContent = data.currentConditions.uvindex;
 
+        renderWeeklyForecast(data);
     } catch (error) {
         alert("Unable to fetch weather data. Please check the city name.");
         console.error(error);
     }
+}
+
+function renderWeeklyForecast(data) {
+    dailyContainer.innerHTML = "";
+
+    data.days.slice(0, 7).forEach(day => {
+        const dayCard = document.createElement("div");
+        dayCard.className = "day-card";
+
+        const dateObj = new Date(day.datetime);
+        const date = dateObj.toLocaleDateString("en-IN", {
+            day: "numeric",
+            month: "short"
+        });
+
+        const temp = Math.round(day.temp);
+        const condition = day.conditions;
+        const icon = day.icon;
+
+        const dayCardIcon = document.createElement("img");
+        dayCardIcon.src = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/PNG/1st%20Set%20-%20Color/${icon}.png`;
+        dayCardIcon.alt = condition;
+        dayCardIcon.classList.add("day-icon");
+
+        dayCard.appendChild(dayCardIcon);
+
+        const dayCardContent = document.createElement("div");
+        dayCardContent.classList.add("day-content");
+        dayCardContent.innerHTML = `
+            <div class="day-temp">${temp}°F</div>
+            <div class="day-date">${date}</div>
+            <div class="day-condition">${condition}</div>
+        `;
+        dayCard.appendChild(dayCardContent);
+        weeklyForecast.appendChild(dayCard);
+    })
 }
